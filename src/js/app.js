144cc717825fa0d237ba7451ddad4fed71a45cdf
Exequiel Dominguez <100177104+Exequiiel021@users.noreply.github.com>
2024-04-10 23:05:19 -0300

    pagina bienes raices terminada con el modo oscuro

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,24 @@ document.addEventListener('DOMContentLoaded', function(){
 
 /*MODO OSCURO*/
 function darkmode(){
+    /*tome automaticamente el modo oscuro de acuerdo a la configuracion del usuario*/
+
+    const prefiereTemaClaro = window.matchMedia('(prefers-color-scheme: light)');
+    if (prefiereTemaClaro.matches) {
+        document.body.classList.remove('dark-mode');
+    } else {
+        document.body.classList.add('dark-mode');
+    }
+
+    prefiereTemaClaro.addEventListener('change', function () {
+        if (prefiereTemaClaro.matches) {
+            document.body.classList.remove('dark-mode');
+        } else {
+            document.body.classList.add('dark-mode');
+        }
+    });
+    /*fin del automatico darkmode de acuerdo al sistema*/
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
 
     botonDarkMode.addEventListener('click', function(){
